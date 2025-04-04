a4a00e919dfe8d3a45667b711e7638670d9f8550
Kweku-Kai <82861988+Kweku-Kai@users.noreply.github.com>
2025-04-04 15:58:37 +0000

    updated backend code
    I added some new alerts which would be printed to the console when there are some specific errors

--- Backend/controllers/product.controller.js
+++ Backend/controllers/product.controller.js
@@ -48,14 +48,18 @@ export const updateProduct = async(req, res) => {
 
 export const deleteProduct = async(req, res) => {
     const {id} = req.params; // get the id from the url
+
+    // check if the product id exists or not
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: 'Invalid Product Id' });
+    }
     
     try {
         await Product.findByIdAndDelete(id); // find the product by id and delete it
         res.status(200).json({ success: true, message: 'Product deleted successfully' });
     } 
     catch (error) {
-        console.error("Error in deleting product:", error.message); 
-        res.status(404).json({ success: false, message: 'Product not found' });   
-        //return res.status(500).json({ success: false, message: 'Server Error' });
+        console.error("Error in deleting product:", error.message);   
+        res.status(500).json({ success: false, message: 'Server Error' });
     }
 }
--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = proccess.env.PORT || 5000
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
@@ -17,5 +18,5 @@ app.use("/api/products",productRoutes)
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:"+ PORT);
 })
